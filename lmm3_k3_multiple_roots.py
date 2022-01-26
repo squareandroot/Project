11385158dbf8238aa45810684d59ec047f0ddd90
squareandroot <77972840+squareandroot@users.noreply.github.com>
2022-01-26 15:16:59 +0100

    Add plot for method with multiple root

---
+++ lmm3_k3_multiple_roots.py
@@ -0,0 +1,15 @@
+import numpy as np
+from runge_kutta_k4 import RK4
+
+
+def LMM3_multiple_roots(t0, tn, n, y0, f):
+    h = abs(tn - t0) / n
+    t = np.linspace(t0, tn, n+1)
+    y = np.zeros(n+1)
+    y[:3] = RK4(t0, t0 + 2*h, 2, y0, f)
+    K1 = f(t[1], y[1])
+    for i in range(2, n):
+        K2 = K1
+        K1 = f(t[i], y[i])
+        y[i+1] = h * (2 * K1 - 2 * K2) + y[i] + y[i-1] - y[i-2]
+    return y
